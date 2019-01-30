f37e62b105f7504a1a5903213b86c85bedbb3015
Muhammad Hewedy <mhewedy@gmail.com>
2019-01-31 00:12:09 +0300

    clean

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,15 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springrouter.EnableRouter;
 
 @SpringBootApplication
-@EnableScheduling
 @EnableRouter(controllerPackage = "com.example.demo.controllers",
         modelPackage = "com.example.demo.model")
-public class DemoApplication implements WebMvcConfigurer {
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
